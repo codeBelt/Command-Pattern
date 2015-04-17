a7bdecef20b9569f77543f2d4bd14748ad51d639
codeBelt <code@codebelt.com>
2015-04-16 23:37:21 -0500

    Add StructureJS

---
+++ src/assets/vendor/structurejs/docs/api.js
@@ -0,0 +1,83 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ApplicationCacheController",
+        "ApplicationCacheEvent",
+        "BaseEvent",
+        "BaseObject",
+        "BrowserUtil",
+        "Collection",
+        "ComponentFactory",
+        "DOMElement",
+        "DateUtil",
+        "DisplayObjectContainer",
+        "EventBroker",
+        "EventDispatcher",
+        "Extend",
+        "LocalStorageController",
+        "LocalStorageEvent",
+        "MathUtil",
+        "MerchantUtil",
+        "NetworkMonitor",
+        "NetworkMonitorEvent",
+        "NumberUtil",
+        "ObjectManager",
+        "Route",
+        "Router",
+        "RouterEvent",
+        "Stage",
+        "StringUtil",
+        "TemplateFactory",
+        "Timer",
+        "TimerEvent",
+        "Util",
+        "ValidationUtil",
+        "ValueObject"
+    ],
+    "modules": [
+        "StructureJS",
+        "controller",
+        "core",
+        "event",
+        "model",
+        "util",
+        "view"
+    ],
+    "allModules": [
+        {
+            "displayName": "controller",
+            "name": "controller",
+            "description": "The ApplicationCacheController..."
+        },
+        {
+            "displayName": "core",
+            "name": "core",
+            "description": "The {{#crossLink \"BaseObject\"}}{{/crossLink}} class is an abstract class that provides common properties and functionality for all StructureJS classes."
+        },
+        {
+            "displayName": "event",
+            "name": "event",
+            "description": "The ApplicationCacheEvent ...."
+        },
+        {
+            "displayName": "model",
+            "name": "model",
+            "description": "The Collection class provides a way for you to manage your models."
+        },
+        {
+            "displayName": "StructureJS",
+            "name": "StructureJS"
+        },
+        {
+            "displayName": "util",
+            "name": "util",
+            "description": "A helper class to detect OS and browsers."
+        },
+        {
+            "displayName": "view",
+            "name": "view",
+            "description": "The {{#crossLink \"DOMElement\"}}{{/crossLink}} class is the base view class for all objects that can be placed into the HTML DOM."
+        }
+    ]
+} };
+});
